41a21f08b926983a7e3b40eaa7742529b1fac9be
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-05-25 21:26:08 +0300

    add:)react project

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+export default function header() {
+  return (
+    <section className='header-w'>
+    <header className='header'>
+      <div header__inner>
+        
+      </div>
+    </header>
+    </section>
+  )
+};
